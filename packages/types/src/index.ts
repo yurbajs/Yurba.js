5d2b82c5584ef2e6986a75c5292dba6f7e67528c
RastGame <rastgame1@gmail.com>
2025-07-21 23:13:56 +0300

    coorect

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,6 +3,6 @@ export * from './core';
 export * from './api';
 export * from './posts';
 export * from './attachments';
-export * from './messages';
+export * from './dialogs';
 export * from './interfaces';
 export * from './errors';
